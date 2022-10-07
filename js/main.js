303f3e79f25c1a7da21e6ad71a56453f35989888
pravin satpute <pravin96satpute@gmail.com>
2022-10-07 18:28:51 +0530

    fass43e

--- js/main.js
+++ js/main.js
@@ -4,3 +4,30 @@ const navLink = document.querySelector('.nav__list');
 hamburger.addEventListener('click', () => {
   navLink.classList.toggle('hide');
 });
+
+$(document).ready(function(){
+  $('.my-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    speed: 300,
+    infinite: true,
+    autoplaySpeed: 5000,
+    autoplay: false,
+    responsive: [
+  {
+    breakpoint: 991,
+    settings: {
+      slidesToShow: 3,
+    }
+  },
+  {
+    breakpoint: 767,
+    settings: {
+      slidesToShow: 1,
+    }
+  }
+]
+  });
+});
